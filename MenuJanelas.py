15b7891e837484ddaf962bbc5ebdf867e61c4d3a
Renato Barbosa <98436200+ORenatoBarbosa@users.noreply.github.com>
2022-03-27 12:28:45 -0300

    Create MenuJanelas.py

---
+++ MenuJanelas.py
@@ -0,0 +1,58 @@
+from math import ceil
+from tkinter.constants import CENTER
+import PySimpleGUI as sg
+
+TITULO = "BEM VINDO AO SEU BANCO PIX!"\
+         "Conta Bancária"
+
+def make_win_login() -> sg.Window:
+    layout_login = [
+        [sg.Text("Nome: "), sg.Input(k="Nome", s=(100, 100))],
+        [sg.Text("Saldo Inicial: "), sg.Input(k="SaldoIni", s=(20, 20))],
+        [sg.Button("Inserir", bind_return_key=True), sg.Exit("Sair")]]
+
+    return sg.Window(TITULO, layout_login)
+
+
+def make_win_menu() -> sg.Window:
+    layout_menu = [
+        [sg.Text("Escolha uma opção:", justification=CENTER)],
+        [sg.Button("Sacar"), sg.Button("Depositar"),sg.Button("Extratos"),sg.Button("Contracheques"),sg.Button("Transferência")],
+        [sg.Button("Abrir conta conjunta"), sg.Button("Minha conta")],
+        [sg.Exit("Sair")]]
+
+    return sg.Window(TITULO, layout_menu)
+
+
+def make_win_sacar():
+    layout_sacar = [
+        [sg.Text("Digite a quantia que deseja sacar: "), sg.Input(k="SacaQnt", s=(8, 10))],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_sacar)
+
+
+def make_win_depositar():
+    layout_depositar = [
+        [sg.Text("Digite a quantia que deseja depositar: "), sg.Input(k="DeposQnt", s=(8, 10))],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_depositar)
+
+def make_win_transf():
+    layout_transf = [
+        [sg.Text("Digite a quantia que deseja transferir: "), sg.Input(k="TransfQnt", s=(8, 10))],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_transf)
+
+
+def make_win_contaconj():
+    layout_contaconj = [
+        [sg.Text("Digite o nome do seu parceiro: "), sg.Input(k="NomeParc", s=(30, 30))],
+        [sg.Text("Digite o saldo inicial do seu parceiro: "), sg.Input(k="SaldoParc", s=(8, 10))],
+        [sg.Button("Inserir", bind_return_key=True), sg.Cancel("Cancelar")]]
+
+    return sg.Window(TITULO, layout_contaconj)
+  
+  # Renato Barbosa
